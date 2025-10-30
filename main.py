a65a51a1f8aacd767700508ae5e701ff5fee648f
ndexcompany4evry1-byte <ndexcompany4evry1@gmail.com>
2025-10-30 02:48:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+import telebot
+import os
+
+TOKEN = os.getenv("TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "👋 مرحبا زاكي! بوتك شغال بنجاح 🎯")
+
+bot.infinity_polling()
